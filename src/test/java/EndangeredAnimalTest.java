085c01cb09bd0fa731daef8feeef05b3bbce6354
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-10-05 12:16:30 -0700

    write and pass test for getId of endangeredAnimal

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,17 +1,30 @@
 import org.junit.*;
-import static org.junit.Assert.*;
 import org.sql2o.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
 public class EndangeredAnimalTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
   @Test
-  public void endangeredAnimal_instantiatesCorrectly_true() {
+  public void Animal_instantiatesAsEndangeredAnimalCorrectly_true() {
     EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Dodo", "healthy", "adult");
     assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
+  }
+
+  @Test
+  public void getHealth_returnsAnimalHealth_true() {
+    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Dodo", "healthy", "adult");
+    assertEquals("healthy", testEndangeredAnimal.getHealth());
+  }
+
+  @Test
+  public void save_ObjectInstantiatesAndSavesWithId_true() {
+    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Dodo", "healthy", "adult");
+    testEndangeredAnimal.save();
+    assertTrue(testEndangeredAnimal.getId() > 0);
   }
 }
